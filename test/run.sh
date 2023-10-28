44e289f46013d9011a7dc28b8b5f0cad9e3ddb15
paulc <212168+paulc@users.noreply.github.com>
2023-10-28 20:41:45 +0000

    tests

--- test/run.sh
+++ test/run.sh
@@ -19,29 +19,44 @@ fi
 export ZROOT="${ZROOT:-zroot}"
 export ZJAIL="$(printf '/tmp/zjail-test %s' $(/usr/bin/od -v -An -N4 -tx4 /dev/urandom))"
 
+# Setup datasets
 ./bin/zjail create_zfs_datasets
+
+# Add cleanup trap 
 trap "./bin/zjail list_instances | xargs -n1 ./bin/zjail destroy_instance; _run /sbin/zfs destroy -Rf \'${ZROOT}${ZJAIL}\'" EXIT
 
+# fetch release
 ./bin/zjail fetch_release "${OS_RELEASE}"
+
+# Create 2 bases 
 ./bin/zjail create_base b1
 ./bin/zjail update_base b1
 ./bin/zjail clone_base b1 b2
 
+# Check chroot_base / jexec_base
 VER1=$(./bin/zjail chroot_base b1 /bin/freebsd-version)
 VER2=$(echo /bin/freebsd-version | ./bin/zjail jexec_base b2)
-
 test "${VER1}" = "${VER2}"
 
+# Non persistent instance
+ID=$(./bin/zjail create_instance b2 -r /bin/freebsd-version)
+./bin/zjail stop_instance $ID
+./bin/zjail start_instance $ID
+./bin/zjail destroy_instance $ID
+
+# Persistent instance
 ID=$(./bin/zjail create_instance b2 -j persist -r /bin/freebsd-version)
 ./bin/zjail stop_instance $ID
 ./bin/zjail start_instance $ID
 ./bin/zjail destroy_instance $ID
 
+# Create 2 persistent instances
 ./bin/zjail create_instance b1 -j persist 
-./bin/zjail create_instance b1 -j persist
+./bin/zjail create_instance b2 -j persist
 
 ./bin/zjail list_instances
 ./bin/zjail list_instances | xargs -n1 ./bin/zjail stop_instance
 ./bin/zjail list_instances | xargs -n1 ./bin/zjail start_instance
 ./bin/zjail list_instances | xargs -n1 ./bin/zjail destroy_instance
+
 
